8920f49531ede8d81131397dcb0835467bdc972d
AmitKadam <amit.bk03@gmail.com>
2020-02-01 10:37:23 +0530

    Migrations edited and executed

--- database/migrations/2020_02_01_035237_create_wallets_table.php
+++ database/migrations/2020_02_01_035237_create_wallets_table.php
@@ -18,8 +18,8 @@ class CreateWalletsTable extends Migration
 
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('order_id')->unsigned();
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
 
             $table->double('amount');
             $table->string('narration')->nullable();
--- database/migrations/2020_02_01_040043_create_tag_types_table.php
+++ database/migrations/2020_02_01_040043_create_tag_types_table.php
@@ -14,7 +14,7 @@ class CreateTagTypesTable extends Migration
     public function up()
     {
         Schema::create('tag_types', function (Blueprint $table) {
-            $$table->bigIncrements('id');
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('desc')->nullable();
         });
