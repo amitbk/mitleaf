42547a1ace85355f49991744505c7d2d74d47991
AmitKadam <amit.bk03@gmail.com>
2020-02-02 18:40:18 +0530

    Image model, migrations and controller created

---
+++ database/migrations/2020_02_02_130713_create_images_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('url');
+            $table->timestamps();
+        });
+        Schema::table('users', function (Blueprint $table) {
+          $table->foreign('avatar_id')->references('id')->on('images')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+        Schema::table('users', function (Blueprint $table) {
+          $table->dropForeign(['avatar_id']);
+        });
+    }
+}
