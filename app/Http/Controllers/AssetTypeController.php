a7bcb84044859eb98cde7cb2bb544820a28a2087
AmitKadam <amit.bk03@gmail.com>
2020-02-02 19:32:29 +0530

    Factory, seeder, model and controller created

---
+++ app/Http/Controllers/AssetTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AssetType;
+use Illuminate\Http\Request;
+
+class AssetTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AssetType  $assetType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AssetType $assetType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AssetType  $assetType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AssetType $assetType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AssetType  $assetType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AssetType $assetType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AssetType  $assetType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AssetType $assetType)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/OrderPlanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\OrderPlan;
+use Illuminate\Http\Request;
+
+class OrderPlanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\OrderPlan  $orderPlan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OrderPlan $orderPlan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\OrderPlan  $orderPlan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OrderPlan $orderPlan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\OrderPlan  $orderPlan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OrderPlan $orderPlan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\OrderPlan  $orderPlan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OrderPlan $orderPlan)
+    {
+        //
+    }
+}
