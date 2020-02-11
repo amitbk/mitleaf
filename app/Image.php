24a1335ed615b10987fbe591e05bc430dcf3f59d
AmitKadam <amit.bk03@gmail.com>
2020-02-11 23:01:59 +0530

    Image created from base64 data and saved in image model

--- app/Image.php
+++ app/Image.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Auth;
 class Image extends Model
 {
-    //
+    public function create_from_base64($data, $path)
+    {
+        // upload image
+        $img = explode(',', $data);
+        $ini =substr($img[0], 11);
+        $type = explode(';', $ini);
+        $image = str_replace(' ', '+', $img[1]);
+        // $path = "images/templates/";
+        $imageName = Auth::id()."_".date('yymd_his').".". $type[0];
+        // return $imageName;
+        file_put_contents($path.$imageName, base64_decode($image));
+        $this->url = $path.$imageName;
+        $this->save();
+    }
 }
