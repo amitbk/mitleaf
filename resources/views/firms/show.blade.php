ade76ddccd500bf434eb7e38b9bd7001c92234f5
AmitKadam <amit.bk03@gmail.com>
2020-02-20 08:14:56 +0530

    Firm show view style changes

--- resources/views/firms/show.blade.php
+++ resources/views/firms/show.blade.php
@@ -8,7 +8,7 @@
                     <div class="col-md-3 mt-5 mx-auto">
                         <img class="img-fluid p-2" src="{{asset( $firm->logo() )}}" alt="">
                     </div>
-                    <div class="col-md-12 my-4">
+                    <div class="col-md-12 my-4 font-weight-bold">
                         <h3>{{$firm->name}}</h3>
                     </div>
             </div>
@@ -25,12 +25,12 @@
                     <div class="card my-2">
 
                         <!-- top -->
-                        <div class="media p-2 border-b">
+                        <div class="media p-2 border_b">
                           <img src="https://picsum.photos/50" alt="John Doe" class="mr-2 rounded-circle">
                           <div class="media-body">
                             <div class="font-weight-bold">John Doe <small class="text-secondary">Posted on February 19, 2016</small></div>
                             <div class="text-secondary">
-                                Hello
+                                <span class="fl_tag">Hello</span>
                             </div>
                           </div>
                         </div>
@@ -38,13 +38,13 @@
                         <div class="card_content p-2">
                             hello
                         </div>
-                        <div class="card_media">
+                        <div class="card_media border_b">
                             <img class="img-fluid w-100" src="https://picsum.photos/500/300" alt="">
                         </div>
                         <div class="card_options p-2">
-                            <button class="btn btn-default btn-sm border-f" type="button" name="button"><i class="fas fa-sync"></i> Recreate</button>
+                            <button class="btn btn-default btn-sm border_f" type="button" name="button"><i class="fas fa-sync"></i> Recreate</button>
 
-                            <button class="btn btn-default btn-sm border-f float-right" type="button" name="button"><i class="fas fa-cloud-download-alt"></i> Download</button>
+                            <button class="btn btn-default btn-sm border_f float-right" type="button" name="button"><i class="fas fa-cloud-download-alt"></i> Download</button>
                         </div>
                     </div>
 
