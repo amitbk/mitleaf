1daaacad31ece653991c32ed09da06943fb4017a
Amit Kadam <amit.bk03@gmail.com>
2021-01-23 21:48:53 +0530

    1 design is final now for frame card on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,42 +13,8 @@
         </div>
         <div class="col-md-9">
 
-          <div class="card shadow-sm mb-2">
-            <div class="card-body p-0">
-
-              <div class="d-flex flex-sm-row">
-                <!-- date -->
-                <div class="card__date_wrapper p-3 bg-lightcyan">
-                  <div class="card__date f-50 ">
-                    23
-                  </div>
-                  <div class="card__date">
-                    Jan
-                  </div>
-                </div>
-
-                <!-- business and image -->
-                <div class="">
-                  <img class="img-fluid shadow h-100" src="https://picsum.photos/100/80" alt="">
-                </div>
-                <div class="flex-grow-1 p-2">
-                    <div class="card__business_meta">
-                      <span class="font-weight-bold">Kere Patil Machinery</span>
-                      <span class="text-secondary">Business post</span>
-                    </div>
-
-                    <div class="">
-                    </div>
-                </div>
-
-              </div>
-
-            </div>
-          </div>
-
-
           <!-- 2 -->
-          @for($i=0; $i<10; $i++)
+          @for($i=1; $i<30; $i++)
           <div class="card shadow-sm mb-3">
             <div class="card-body p-0">
 
@@ -57,7 +23,7 @@
 
                   <div class="card__date_wrapper p-3 bg-lightcyan h-100">
                     <div class="card__date f-50 ">
-                      23
+                      {{$i}}
                     </div>
                     <div class="card__date">
                       Jan
