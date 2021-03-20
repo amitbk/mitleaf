96746a645990c0809538c95a00c2111d95b3559e
Amit Kadam <amit.bk03@gmail.com>
2021-03-20 15:06:06 +0530

    Template show page updated

--- resources/views/emails/clients/order_ended.blade.php
+++ resources/views/emails/clients/order_ended.blade.php
@@ -15,4 +15,6 @@ Renew Again
 
 Thanks,<br>
 {{ config('app.name') }}
+
+<small> If you don't want to receive this mails, Click <a href="{{url('/unsubscribe')}}">here</a> to Unsubscribe.</small>
 @endcomponent
--- resources/views/templates/show.blade.php
+++ resources/views/templates/show.blade.php
@@ -18,23 +18,39 @@
             <div class="card_media border_b">
                 <img class="img-fluid w-100" src="{{asset($template->image->url)}}" alt="">
             </div>
+
+            <div class="d-flex my-3">
+
+
+              <div class="">
+                <form class="form-delete col p-1" style="display: unset" method="post" action="{{ route('templates.destroy', $template->id) }}">
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit" class="btn btn-danger border_f" onclick="return confirm('Are you sure to delete this template?')"><i class="fas fa-trash"></i> Delete</button>
+                </form>
+              </div>
+
+
+
+            </div>
+
           </div>
           <div class="col-md-8 my-2" >
+
+
+
             <form class="" action="{{url('templates/'.$template->id.'/styles')}}" method="post">
               @csrf
               <div class="d-flex mb-3">
-                <button type="submit" class="btn btn-success">Update</button>
                 <a href="{{url('templates/'.$template->id.'/test/3?test=1&location=bottom&ratio=35&x_axis=20&y_axis=20')}}" target="_blank" class="btn btn-primary border_f ml-2">Test</a>
+                <div class="ml-auto">
+                  <a href="{{ route('templates.index')}}" class="btn btn-outline-primary">Lists</a>
 
-                <div class="ml-2">
-                  <form class="form-delete col p-1" style="display: unset" method="post" action="{{ route('templates.destroy', $template->id) }}">
-                      @method('DELETE')
-                      @csrf
-                      <button type="submit" class="btn btn-danger border_f" onclick="return confirm('Are you sure to delete this template?')"><i class="fas fa-trash"></i> Delete</button>
-                  </form>
+                  <a href="{{route('templates.create')}}" class="btn btn-primary ml-auto">Add New</a>
+                  <button type="submit" class="btn btn-success ml-auto">Update</button>
                 </div>
 
-                <a href="{{route('templates.create')}}" class="btn btn-primary ml-auto">Add New</a>
+
               </div>
 
               <div class="border p-2">
